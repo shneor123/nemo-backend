8630cde3bdd0e9a7809f29263cae67b612eba111
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-12-14 11:04:56 +0200

    add bild from front

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ if (process.env.NODE_ENV === 'production') {
     }
     app.use(cors(corsOptions))
 }
-
+// routes
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const boardRoutes = require('./api/board/board.routes')
 const { setupSocketAPI } = require('./services/socket.service')
 
-// routes
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/board', boardRoutes)
@@ -38,7 +37,7 @@ setupSocketAPI(http)
 
 
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 const PORT = process.env.PORT || 3030
