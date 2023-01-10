c2e381a85b85617a066e6606b7096f7482f7d311
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2023-01-10 17:31:30 +0200

    to render

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // dbURL:'mongodb://localhost:27017'
+  dbURL:'mongodb://localhost:27017'
   // 'dbURL': 'mongodb+srv://omri:vgv02RlV26fL52KB@cluster0.tm2tn.mongodb.net/?retryWrites=true&w=majority',
-  dbURL:'mongodb+srv://omri:vgv02RlV26fL52KB@cluster0.tnv1v.mongodb.net/?retryWrites=true&w=majority'
+  // dbURL:'mongodb+srv://omri:vgv02RlV26fL52KB@cluster0.tnv1v.mongodb.net/?retryWrites=true&w=majority'
 }
