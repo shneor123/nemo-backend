aa06bed83170b1715b26b5d35c5d23ee62251ae2
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2023-01-12 15:11:40 +0200

    deploy to render

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,4 @@
 module.exports = {
   // dbURL:'mongodb://localhost:27017'
   dbURL: "mongodb+srv://shneor:a3P1J46W53R6vQEj@cluster0.e7tvfrv.mongodb.net/?retryWrites=true&w=majority"
-
-  // 'dbURL': 'mongodb+srv://omri:vgv02RlV26fL52KB@cluster0.tm2tn.mongodb.net/?retryWrites=true&w=majority',
-  // dbURL:'mongodb+srv://omri:vgv02RlV26fL52KB@cluster0.tnv1v.mongodb.net/?retryWrites=true&w=majority'
 }
