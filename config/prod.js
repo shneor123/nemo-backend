720e62f34098bd1cd08b2ac471810db2692fee1a
shneor <shneor.333@gmail.com>
2025-07-13 13:42:47 +0300

    change mongodb

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   // dbURL:'mongodb://localhost:27017'
-  dbURL: "mongodb+srv://shneor:a3P1J46W53R6vQEj@cluster0.e7tvfrv.mongodb.net/?retryWrites=true&w=majority"
+     dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net"
+
+//  dbURL: "mongodb+srv://shneor:a3P1J46W53R6vQEj@cluster0.e7tvfrv.mongodb.net/?retryWrites=true&w=majority"
 }
