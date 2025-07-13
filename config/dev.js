19100c26dfdc945bf3491e63e2b1577dcce7111f
shneor <shneor.333@gmail.com>
2025-07-13 15:04:31 +0300

    deploy to render

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
   // dbURL: 'mongodb://localhost:27017',
-   dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net"
+dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/board_db?retryWrites=true&w=majority"
 }
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // dbURL:'mongodb://localhost:27017'
-     dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net"
-
-//  dbURL: "mongodb+srv://shneor:a3P1J46W53R6vQEj@cluster0.e7tvfrv.mongodb.net/?retryWrites=true&w=majority"
+  // dbURL: 'mongodb://localhost:27017',
+dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/board_db?retryWrites=true&w=majority"
 }
