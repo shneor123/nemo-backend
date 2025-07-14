52651d1bed2cc414cd512e34cff04bb4abd46ae2
shneor <shneor.333@gmail.com>
2025-07-14 14:31:04 +0300

    new version fro front react to public

--- services/db.service.js
+++ services/db.service.js
@@ -2,7 +2,8 @@ const { MongoClient } = require('mongodb');
 const logger = require('../services/logger.service');
 const config = require('../config');
 
-const dbURL =  "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/board_db?retryWrites=true&w=majority";
+const dbURL =
+  'mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/board_db?retryWrites=true&w=majority';
 const dbName = 'board_db';
 
 let dbConn = null;
@@ -10,33 +11,36 @@ let dbConn = null;
 console.log('🌐 Connecting to DB with URL:', dbURL);
 
 async function getCollection(collectionName) {
-    try {
-        const db = await connect();
-        console.log(`📂 Fetching collection: ${collectionName}`);
-        return db.collection(collectionName);
-    } catch (err) {
-        logger.error('❌ Failed to get Mongo collection', err);
-        throw err;
-    }
+  try {
+    const db = await connect();
+    console.log(`📂 Fetching collection: ${collectionName}`);
+    return db.collection(collectionName);
+  } catch (err) {
+    logger.error('❌ Failed to get Mongo collection', err);
+    throw err;
+  }
 }
 
 async function connect() {
-    if (dbConn) return dbConn;
-    try {
-        const client = await MongoClient.connect(dbURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        const db = client.db(dbName);
-        dbConn = db;
-        console.log('✅ Connected to DB!');
-        return db;
-    } catch (err) {
-        logger.error('❌ Cannot Connect to DB', err);
-        throw err;
-    }
+  if (dbConn) return dbConn;
+
+  try {
+    const client = new MongoClient(dbURL, {
+      ssl: true,
+      tlsAllowInvalidCertificates: false,
+      tlsAllowInvalidHostnames: false,
+    });
+
+    await client.connect();
+    dbConn = client.db(dbName);
+    console.log('✅ Connected to DB!');
+    return dbConn;
+  } catch (err) {
+    logger.error('❌ Cannot Connect to DB', err);
+    throw err;
+  }
 }
 
 module.exports = {
-    getCollection,
+  getCollection,
 };
