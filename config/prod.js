26725b3e83019f7cf0e5cea1bbe823460f5dc33a
shneor <shneor.333@gmail.com>
2025-07-14 14:36:14 +0300

    new version fro front react to public

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,6 @@
 
 module.exports = {
 
- dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/board_db?retryWrites=true&w=majority"
+dbURL: "mongodb+srv://shneor333:Y0bfbbrNir35vTi4@nemonew.djdu6sc.mongodb.net/"
 
 }
